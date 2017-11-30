22716720c159f221b30e0370a7fdc23673b43ca5
azobboza <b.trifunovic@sks365.com>
2017-11-30 17:32:52 +0100

    Create Add a Gig form

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,31 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public GigsController()
+        {
+            //questions
+            //1. What about dependency injection
+            //2. What about Repository patttern
+            _context = new ApplicationDbContext();
+        }
+        
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel 
+            { 
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+	}
+}
---
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using GigHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Vemue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int GenreId { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
