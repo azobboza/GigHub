b8c55f73c45a4b403e305ce8e9b54577c4f75512
Bozidar Trifunovic <bozidar.trifunovic85@gmail.com>
2018-11-14 00:16:01 +0100

    Working on testing repository.

--- GigHub/Persistance/Repositories/GigRepository.cs
+++ GigHub/Persistance/Repositories/GigRepository.cs
@@ -11,8 +11,8 @@ namespace GigHub.Persistance.Repositories
 {
     public class GigRepository : IGigRepository
     {
-        private readonly ApplicationDbContext _context;
-        public GigRepository(ApplicationDbContext context)
+        private readonly IApplicationDbContext _context;
+        public GigRepository(IApplicationDbContext context)
         {
             _context = context;
         }
